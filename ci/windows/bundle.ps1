8e83df161625cd19cddd12b5049f9fe07d3c842f
Patrik Fiedler <patrik.fiedler@nexenio.com>
2017-10-26 18:00:13 +0200

    delete unused loc

--- ci/windows/bundle.ps1
+++ ci/windows/bundle.ps1
@@ -5,7 +5,6 @@ if (Test-Path -Path usr) {
 New-Item -ItemType  Directory -Force -Path usr/local/lib
 New-Item -ItemType  Directory -Force -Path usr/local/include
 Copy-Item build/src/Release/* usr/local/lib/ -Force -Recurse
-Copy-Item includes\ include\nsfw -recurse
 Copy-Item include        usr/local/     -Force -Recurse
 
 # compress
